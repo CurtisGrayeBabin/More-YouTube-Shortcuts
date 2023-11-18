4724164e5be81cec3046b997f09fa1cdbaa1d32b
Curtis Babin <curtsocal@gmail.com>
2023-11-18 13:36:38 -0800

    init

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const videoElement = document.querySelector('.html5-video-container > video');
+
+// get like or dislike button
+// tpye: string either 'like' or 'dislike' 
+const getLDButton = (type) => {
+    return document.getElementById(`segmented-${type}-button`).querySelector('button');
+}
+
+// capture keypresses
+document.body.addEventListener('keydown', (event) => {
+    // skip 30 seconds when pressing 's' key
+    if (event.key === 's') {
+        const newTime = videoElement.currentTime + 30;
+        videoElement.currentTime = Math.min(newTime, videoElement.duration);
+    } 
+    // remove YouTube warning message below video player
+    else if (event.key === 'r'){
+        if (document.getElementById('clarify-box')) 
+            document.getElementById('clarify-box').remove();
+    }
+    // like video with u key
+    else if (event.key === 'u'){
+        getLDButton('like').click();
+    } 
+    // dislike video with d key
+    else if (event.key === 'd'){
+        getLDButton('dislike').click();
+    }
+    // remove like or dislike with u key
+    else if (event.key === 'n'){
+        const likeButton = getLDButton('like');
+        
+        // if like button has been clicked then click again to undo it
+        if (likeButton.getAttribute('aria-pressed') === "true"){
+            likeButton.click();
+            return;
+        } 
+        
+        const dislikeButton = getLDButton('dislike');
+        // if like button has been clicked then click again to undo it
+        if (dislikeButton.getAttribute('aria-pressed') === "true"){
+            dislikeButton.click();
+        } 
+        
+
+    }
+    
+});
